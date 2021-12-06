765a3090a893c32158b210c6f6ef45119adff4e0
Eric Liao <rcliao01@gmail.com>
2021-12-06 12:21:04 -0800

    feat: add quata for concurrency

--- app/service/sales-api/main.go
+++ app/service/sales-api/main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
+
+	"github.com/rcliao/gc-2021-ultimate-service/foundation/logger"
+	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
 )
 
 var build = "develop"
 
 func main() {
-	log.Println("starting service TEST. build:", build)
-	defer log.Println("shutdown")
 
-	// to receive the shutdown signal from Kubernetes
+	// construct the application logger
+	log, err := logger.New("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		log.Sync()
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	// =========================================================================
+	// GOMAXPROCS
+
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas.
+	if _, err := maxprocs.Set(); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 	<-shutdown
 
-	log.Println("starting shutdown")
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/rcliao/gc-2021-ultimate-service
 
 go 1.17
 
-require go.uber.org/zap v1.19.1
+require (
+	go.uber.org/automaxprocs v1.4.0
+	go.uber.org/zap v1.19.1
+)
 
 require (
 	go.uber.org/atomic v1.7.0 // indirect
