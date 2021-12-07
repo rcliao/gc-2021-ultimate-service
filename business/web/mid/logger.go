dc0c3ce7c8c0ed90aeff0e83363c3dcc0f43eedd
Eric Liao <rcliao01@gmail.com>
2021-12-07 11:50:11 -0800

    feat: add context for trace id in logger

--- business/web/mid/logger.go
+++ business/web/mid/logger.go
@@ -3,6 +3,7 @@ package mid
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/rcliao/gc-2021-ultimate-service/foundation/web"
 	"go.uber.org/zap"
@@ -13,15 +14,24 @@ func Logger(log *zap.SugaredLogger) web.Middleware {
 	m := func(handler web.Handler) web.Handler {
 
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			// If the context is missing this value, request the service
+			// to be shutdown gracefully.
+			v, err := web.GetValues(ctx)
+			if err != nil {
+				// TODO: send signal back to shutdown server
+				log.Infow("HALP", err)
+				return nil
+			}
+
 			// LOG STARTED
-			log.Infow("request started", "method", r.Method, "path", r.URL.Path,
+			log.Infow("request started", "traceid", v.TraceID, "method", r.Method, "path", r.URL.Path,
 				"remoteaddr", r.RemoteAddr)
 
-			err := handler(ctx, w, r)
+			err = handler(ctx, w, r)
 
 			// LOG COMPLETED
-			log.Infow("request completed", "method", r.Method, "path", r.URL.Path,
-				"remoteaddr", r.RemoteAddr)
+			log.Infow("request completed", "traceid", v.TraceID, "method", r.Method, "path", r.URL.Path,
+				"remoteaddr", r.RemoteAddr, "statuscode", v.StatusCode, "since", time.Since(v.Now))
 
 			return err
 		}
--- foundation/web/web.go
+++ foundation/web/web.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 	"os"
 	"syscall"
+	"time"
 
 	"github.com/dimfeld/httptreemux/v5"
+	"github.com/google/uuid"
 )
 
 // A Handler is a type that handles a http request within our own little mini
@@ -49,8 +51,18 @@ func (a *App) Handle(method string, path string, handler Handler, mw ...Middlewa
 	handler = wrapMiddleware(a.mw, handler)
 
 	h := func(w http.ResponseWriter, r *http.Request) {
-		// DO SOMETHING BEFORE
-		if err := handler(r.Context(), w, r); err != nil {
+		// Pull the context from the request and
+		// use it as a separate parameter.
+		ctx := r.Context()
+		// Set the context with the required values to
+		// process the request.
+		v := Values{
+			TraceID: uuid.New().String(),
+			Now:     time.Now(),
+		}
+		ctx = context.WithValue(ctx, key, &v)
+
+		if err := handler(ctx, w, r); err != nil {
 			// TODO
 		}
 		// TO SOMETHING AFTER
